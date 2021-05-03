07a489e6c93deec5de991184e6879beaf47aa0c7
Arnav Kohli <arnavkohli@gmail.com>
2021-05-04 01:19:36 +0530

    adding more logs

--- common/utils.py
+++ common/utils.py
@@ -19,6 +19,7 @@ class TelegramBot:
 
 def get_calendar_by_pin(pincode: int, date: str) -> dict:
 	url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}"
+	print ("url", url)
 	headers = {
 		"user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
 		"accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
