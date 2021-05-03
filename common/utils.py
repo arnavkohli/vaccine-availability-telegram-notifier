c2df50c80c77e0f2163e9a3aea80d0163fa68d42
Arnav Kohli <arnavkohli@gmail.com>
2021-05-04 01:16:48 +0530

    adding headers for test requests

--- common/utils.py
+++ common/utils.py
@@ -19,7 +19,13 @@ class TelegramBot:
 
 def get_calendar_by_pin(pincode: int, date: str) -> dict:
 	url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}"
-	request = requests.get(url)
+	headers = {
+		"user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+		"accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"accept-encoding" : "gzip, deflate, br",
+		"accept-language" : "en-US,en;q=0.9"
+	}
+	request = requests.get(url, headers=headers)
 	print ("api setu response", request.content)
 	if request.status_code == 200:
 		data = request.json()
